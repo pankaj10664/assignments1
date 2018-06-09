3f9d73e0852bd68de7a1e62940819c3d58fe907c
pankaj10664 <pankaj10664@gmail.com>
2018-06-09 15:39:46 +0530

    star triangle pattern

---
+++ program6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Pattern
+{
+public static void main(String args[])
+{
+int i,j;
+int k=4;
+
+for(i=1;i<=k;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
