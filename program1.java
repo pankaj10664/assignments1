faf295373749ed743cc85a3707348ce70fbc0b6d
pankaj10664 <pankaj10664@gmail.com>
2018-06-09 15:24:11 +0530

    add two numbers

---
+++ program1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum
+{
+	public static void main(String args[])
+	{
+Scanner obj=new Scanner(System.in);
+System.out.println("enter two no");
+int a=obj.nextInt();
+int b=obj.nextInt();
+int c;
+c=a+b;
+System.out.println("the sum of two no. is = "+c);
+	}
+}
